3ae20a3f36958db1d8b206b273985f2993002361
Sen20312123 <sen20312123@gmail.com>
2022-10-12 14:06:53 +0700

    th

---
+++ Introduction to Java/src/ThucHanh.java
@@ -0,0 +1,16 @@
+public class ThucHanh {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println( "i =" + i);
+        float f = 20.5f;
+        System.out.println( "f =" + f);
+        double d = 20.5d;
+        System.out.println( "d =" + d);
+        boolean b = true;
+        System.out.println( "b =" + b);
+        char c = 'a';
+        System.out.println( "c =" + c);
+        String s = "Hà Nội";
+        System.out.println( "s =" + s);
+    }
+}
